655688e6a1269f2f955f8e9f5b165f725b278ad7
SumitKmrSingh <sumitkumar203411@gmail.com>
2024-03-16 11:25:47 +0530

    MultipleCompany functionality added

---
+++ EmployeeWage/MultipleCompanyUC8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class MultipleCompanyUC8
+    {
+        public void getEmployeeDetails()
+        {
+            SwitchCaseUC4 switchCaseUC4 = new SwitchCaseUC4();
+            switchCaseUC4.getEmployeeInfo();
+        }
+    }
+}
