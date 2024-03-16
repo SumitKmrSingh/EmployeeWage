a2be20aedfe9d640602ac8f614a69df22ba25760
SumitKmrSingh <sumitkumar203411@gmail.com>
2024-03-16 10:54:21 +0530

    Employee Parttime fucntion addded

---
+++ EmployeeWage/EmployeeWagePartTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class EmployeeWagePartTime
+    {
+        public void getPartTimeWage()
+        {
+            Console.Write("Enter total Part time Working hour : ");
+            int workingHour = Convert.ToInt32(Console.ReadLine());
+     
+            int totalWage = 20 * workingHour;
+            Console.WriteLine("Total Wage : " + totalWage);
+        }
+    }
+}
