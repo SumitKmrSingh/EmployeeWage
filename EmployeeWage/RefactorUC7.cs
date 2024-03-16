31cc06a6b54a4e4302529a477b69ae68a10b377d
SumitKmrSingh <sumitkumar203411@gmail.com>
2024-03-16 11:13:52 +0530

    added RefactorUC7 functionality

---
+++ EmployeeWage/RefactorUC7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class RefactorUC7 
+    {
+        public void getRefactor() 
+        {
+            EmployeeWage employeeWage = new EmployeeWage();
+            Console.WriteLine(employeeWage.getEmployeDailyWage());
+        }
+    }
+}
