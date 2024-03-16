6488bcadb147050ccb1f721fcea3af1c6e965ad8
SumitKmrSingh <sumitkumar203411@gmail.com>
2024-03-16 11:07:06 +0530

    added MonthlyWages functionality

---
+++ EmployeeWage/MonthlyWage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class MonthlyWage
+    {
+        public MonthlyWage() {
+            Console.Write("How many month you have work ? ");
+            int totalMonth = Convert.ToInt32(Console.ReadLine());
+            int totalWage = (20 * totalMonth) * 160;
+            Console.WriteLine("Your Total Monthly Wage : " + totalWage);
+        }
+    }
+}
