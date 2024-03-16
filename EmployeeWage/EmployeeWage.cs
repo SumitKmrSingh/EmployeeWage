44b1fdff24f79408c63216649ab6ca17c5a38bcf
SumitKmrSingh <sumitkumar203411@gmail.com>
2024-03-16 09:17:29 +0530

    EmployeeWage function added

---
+++ EmployeeWage/EmployeeWage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class EmployeeWage
+    {
+        public void getEmployeDailyWage()
+        {
+            Console.Write("Enter Total number of Working hour : ");
+            int workingHour = Convert.ToInt32(Console.ReadLine());
+            double totalDay = workingHour/ 8;
+            int totalWage = 20*workingHour;
+            Console.WriteLine("Total Working Day : "+totalDay);
+            Console.WriteLine("Total Wage : "+totalDay);
+        }
+    }
+}
