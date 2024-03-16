14dd5c9447462d716a6b124333fb99aa6a802438
SumitKmrSingh <sumitkumar203411@gmail.com>
2024-03-16 09:13:34 +0530

    added EmployeeAttendence functionality

---
+++ EmployeeWage/EmployeAttendence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class EmployeAttendence
+    {
+        public void getEmployeeAttendence()
+        {
+            Random random = new Random();
+            int present = random.Next(0, 2);
+            if(present == 1) {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+        }
+    }
+}
